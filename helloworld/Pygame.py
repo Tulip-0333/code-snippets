f8e0daf559d4c5792ae00dcacbbfac9c678ac555
Tulip <93089383+Tulip-0333@users.noreply.github.com>
2023-01-14 14:58:44 -0500

    Create Pygame.py

---
+++ helloworld/Pygame.py
@@ -0,0 +1,29 @@
+import pygame
+
+pygame.init()
+
+window_size = (400, 400)
+
+screen = pygame.display.set_mode(window_size)
+
+pygame.display.set_caption("Hello, World!")
+
+font = pygame.font.Font(None, 36)
+
+text = font.render("Hello, World!", 1, (255, 255, 255))
+
+text_rect = text.geet_rect()
+
+text_rect.center = (200, 200)
+
+running = True
+
+while running:
+  for even in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+  screen.fill((0, 0, 0))
+  sreen.blit(text, text_rect)
+  pygame.display.flip()
+  
+pygame.quit()
